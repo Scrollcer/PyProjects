f12314eb149b48a7d3ccd78217e31cc47f26d920
denslaz <43611625+denslaz@users.noreply.github.com>
2022-07-26 17:06:55 +0500

    Finished Fifth Chapter

---
+++ FifthChapter/ThirdAndFourthTasks.py
@@ -0,0 +1,13 @@
+#--------------------------------------------------------------------------------
+# First task - work with non-nested list, second task - upgrade from non-nested list to nested list
+relativesList = {"Denis": ("Sergey", "Alexander"), "Ivan": ("Alexander", "Efraim"), "Petya": ("Arseniy", "Vsevolod")}
+name = "None"
+while 1:
+    name = input("Please, enter name of a person, whose father's (and grandfather's) name you want to know: ").capitalize()
+    if name == "":
+        print("Goodbye!")
+        break
+    elif name not in relativesList:
+        print("Sorry, I don't know this person.")
+    else:
+        print("Father name: " + relativesList[name][0] + "\nGrandfather name: " + relativesList[name][1])
